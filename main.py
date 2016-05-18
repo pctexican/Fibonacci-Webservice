cd15dcb0f9d440c4dfb0f8c60104842af08319f4
Paul Clifton <pclifton@alumni.utexas.net>
2016-05-18 01:32:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+#!/usr/bin/python
+
+import web
+from fibonacci_service import fibonacci
+
+
+urls = (
+    "/", "index",
+    "/fibonacci/(.*)", "fibonacc"
+)
+
+class index:
+	def GET(self):
+		return 'Fibonacci web service. Append the url with /fibonacci/<number> to generate the series.'
+
+class fibonacc:
+	def GET(self, num):
+		try:
+			num = int(num)
+			series = fibonacci(num)
+		except ValueError as e:
+			return """{\n"Exception":"%s"\n}""" % (e) 
+
+		return "%s" % (series)
+
+def main():
+	serv = web.application(urls, globals())
+	serv.run()
+
+if __name__ == '__main__':
+    main()
 
